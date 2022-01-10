5054b0e77ccd1c8e94e2c048e8acb36816ceab4c
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-01-10 19:16:38 +0300

    Add post request & some styles

--- liba/src/components/header/header.jsx
+++ liba/src/components/header/header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTh } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import './header.scss';
 
 const Header = () => {
     return (
         <header className='header'>
             <div className="container">
-                header
+                <div className="header__wrapper">
+                    <div className="header__leftside">
+                        <FontAwesomeIcon icon={faTh} className="header__leftside_menu-icon"/>
+                        <Link to="/"><div className="logo">Liba_</div></Link>
+                    </div>
+                    <div className="header__rideside">
+
+                    </div>
+                </div>
             </div>
         </header>
     )
--- liba/src/components/pages/allResources/allResources.jsx
+++ liba/src/components/pages/allResources/allResources.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 
 const AllResources = ({actionSection=true}) => {
     const [allResources, setAllResources] = useState([]);
@@ -12,23 +15,37 @@ const AllResources = ({actionSection=true}) => {
         });
     }, []);
 
-    function createPost() {
+    function createResource() {
         axios.post(baseURL, {
-                name: "12345",
-                category: "12345",
+                name: "any program",
+                category: "soft",
+                link: "qwerty",
+                date: Date.now,
             })
                 .then((response) => {
-                    console.log(response.data);
-                // setAllResources(...allResources, response.data);
+                    console.log(allResources);
+                    setAllResources([...allResources, response.data]);
             });
         }
+
+    function editResourse() {
+        axios.put(baseURL, {
+            id: "",
+        })
+    }
+
+    function deleteResourse() {
+        axios.delete(baseURL, {
+            id: "",
+        })
+    }
 
     if (!allResources || allResources.length === 0) return <p>Упс, здесь пока что ничего нет.</p>
     //TODO: Создать универсальный компонент обёртки, принимающий в пропсы children разметку с целью универсанализации окон.
     //TODO: Сделать капсом либа на фоне ресурсов в светло - сером цвете.
     return (
         // TODO: Сделать универсальный компонент.
-        // TODO: При наведении на ресурс появляются кнопки: редактировать, удалить, при наведении на которые они становятся чёрными.
+        // TODO: При наведении на ресурс появляются кнопки: редактировать, удалить, при наведении на которые они становятся чёрными. (Работает только при истинном значении).
         <div className='action-info-wrapper'>
             <div className='allResources__wrapper'>
                 {
@@ -44,7 +61,13 @@ const AllResources = ({actionSection=true}) => {
                                 </div>
                             </div>
                             <div className='section__rightside'>
-                                <div className='section__rightside_category'>{resource.category}</div>
+                                <div className="section__rightside_top">
+                                    <div className='section__rightside_category'>{resource.category}</div>
+                                </div>
+                                <div className="section__rightside_bottom">
+                                    <FontAwesomeIcon icon={faPen} className="edit-section-icon" onClick={editResourse}/>
+                                    <FontAwesomeIcon icon={faTrashAlt} className="delete-section-icon" onClick={deleteResourse}/>
+                                </div>
                             </div>
                         </div>
                     )
@@ -58,7 +81,7 @@ const AllResources = ({actionSection=true}) => {
                         <div className='countOfResources'>{allResources.length}</div>
                         {/* TODO: Сделать форму для создания ресурса.
                         Выести в отдельный компонент. */}
-                        <button onClick={createPost}>create new resourse</button>
+                        <button onClick={createResource}>create new resourse</button>
                     </div>
                 }
             </div>

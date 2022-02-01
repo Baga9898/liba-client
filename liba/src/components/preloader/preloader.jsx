4fab01e95a385869df2f0830ed7521ee089d50c6
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-02-01 23:09:20 +0300

    Delete unnecessary files

--- liba/src/components/preloader/preloader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CookiePreloader from '../../assets/cookie-smile.png';
-
-const Preloader = () => {
-    return <img className='preloader' src={CookiePreloader}/>;
-};
-
-export default Preloader;

c083083c1e38c7e5235b04c46070633fb7890589
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-06-14 18:59:32 +0300

    Delete unnecessary files

--- src/components/utils/preloader/preloader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './preloader.scss';
-import CookiePreloader from '../../../assets/cookie-smile.png';
-
-const Preloader = () => {
-    return <img className='preloader' src={CookiePreloader} alt='Cookie preloader'/>;
-};
-
-export default Preloader;
--- src/redux/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import { countOfResourcesReducer } from '../reducers/countOfResourcesReducer';
-
-const rootReducer = combineReducers({
-    resourcesCount: countOfResourcesReducer,
-})
-
-export const store = createStore(rootReducer);

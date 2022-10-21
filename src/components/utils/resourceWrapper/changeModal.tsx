38d11bb256bf1e54c4f9d7d132da92054c796577
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2022-10-21 22:15:05 +0300

    Add animations for change modal

--- src/components/utils/resourceWrapper/changeModal.tsx
+++ src/components/utils/resourceWrapper/changeModal.tsx
@@ -4,7 +4,7 @@ type ChangeModalProps = {
 
 const ChangeModal: React.FC<ChangeModalProps> = ({ resource }) => {
   return (
-    <div className='changeModal'>
+    <div className='changeModal animate__animated animate__fadeInUp'>
         <p>Changed: <span>Name here</span></p>
         <p>When: <span>21.10.2022</span></p>
     </div>
